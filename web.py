1befb57a0ed332cee29eea5f02063de1d826dc2d
Kevin Fu <jifu@gmail.com>
2018-12-27 19:17:28 +0800

    Draft version for hello

---
+++ web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify
+import socket
+import os
+import getpass
+
+app = Flask(__name__)
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return jsonify({
+		'path': path,
+		'hostname': socket.gethostname(),
+		'ip': get_host_ip(),
+		'uid': os.getuid(),
+		'username': getpass.getuser(),
+		'home': os.environ['HOME'],
+		})
+
+@app.route('/todo', methods=['GET'])
+def get_tasks():
+    return jsonify({'todo': "..."})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
